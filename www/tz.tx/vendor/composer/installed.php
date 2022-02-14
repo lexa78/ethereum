c9c9a0c1dd7a81c160a82a6ec3ff1b6826a1ce3f
Stan <stansmith81028@gmail.com>
2022-02-14 21:44:01 +0300

    init commit

---
+++ www/tz.tx/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'lajosbencz/geth-php' => array(
+            'pretty_version' => '0.2.1',
+            'version' => '0.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../lajosbencz/geth-php',
+            'aliases' => array(),
+            'reference' => '295847f0e639181db5a4d61d6d9b189b96ffda00',
+            'dev_requirement' => false,
+        ),
+    ),
+);
